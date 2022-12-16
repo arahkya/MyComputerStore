6b4e7bec2e10a051415f9d9de3d9ef272cf436d2
arahk <arahk@ARAHK-DESKTOP>
2022-12-16 20:11:54 +0700

    Add project files.

---
+++ MyComputerStore.Web/Models/CardModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyComputerStore.Web.Models
+{
+    public class CardModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
